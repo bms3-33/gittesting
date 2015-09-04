d66c6df7f900bb44899d2b0bbb62629f483c785e
read <hedegang@icloud.com>
2015-09-04 17:24:22 +0800

    Initial Commit

---
+++ gittesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittesting
+//
+//  Created by read on 15/9/4.
+//  Copyright (c) 2015年 read. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittesting
+//
+//  Created by read on 15/9/4.
+//  Copyright (c) 2015年 read. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
